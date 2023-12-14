f31b37430ad029eddb51126902e271626ea7edce
happypineapple3 <gwalton.ryan@gmail.com>
2023-12-14 02:23:47 -0700

    changed sign-in to about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,17 +4,17 @@ import '../styles/About.css';
 
 export function About() {
     return (
-        <div className="container">
-            <div className="signin-container">
-                <h1 className="signin-header">Sign-In</h1>
-                <div className="input-container">
-                    <input type="text" placeholder="Username or Email" className="input-field" />
+        <div className="about">
+            <span>Flavor Town</span>
+            <div className="master-container">
+                <div className="text-container">
+                    <h1 className="header">More about us!</h1>
+                    <div className="description">
+                        Suck my nuts
+                    </div>
+                    <button className="signin-button">Sign In</button>
+                    <p className="signup-link">Not a member? <Link to="/sign_up">Sign up now!</Link></p>
                 </div>
-                <div className="input-container">
-                    <input type="password" placeholder="Password" className="input-field" />
-                </div>
-                <button className="signin-button">Sign In</button>
-                <p className="signup-link">Not a member? <Link to="/sign_up">Sign up now!</Link></p>
             </div>
         </div>
     )
